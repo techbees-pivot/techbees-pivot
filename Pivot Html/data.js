626888dc52e3941007fc764cc7f4358b1246558f
Nishanth555 <67894205+Nishanth555@users.noreply.github.com>
2020-07-09 14:33:20 +0530

    Add files via upload

---
+++ Pivot Html/data.js
@@ -0,0 +1,89 @@
+var sales = [{
+    "orderId": 1,
+    "region": "North America",
+    "country": "USA",
+    "city": "New York",
+    "amount": 1740,
+    "date": "2013/01/06"
+}, {
+    "orderId": 2,
+    "region": "North America",
+    "country": "USA",
+    "city": "Los Angeles",
+    "amount": 850,
+    "date": "2013/01/13"
+}, {
+    "orderId": 3,
+    "region": "North America",
+    "country": "USA",
+    "city": "Denver",
+    "amount": 2235,
+    "date": "2013/01/07"
+}, {
+    "orderId": 4,
+    "region": "North America",
+    "country": "CAN",
+    "city": "Vancouver",
+    "amount": 1965,
+    "date": "2013/01/03"
+}, {
+    "orderId": 5,
+    "region": "North America",
+    "country": "CAN",
+    "city": "Edmonton",
+    "amount": 880,
+    "date": "2013/01/10"
+}, {
+    "orderId": 6,
+    "region": "South America",
+    "country": "BRA",
+    "city": "Rio de Janeiro",
+    "amount": 5260,
+    "date": "2013/01/17"
+}, {
+    "orderId": 7,
+    "region": "South America",
+    "country": "ARG",
+    "city": "Buenos Aires",
+    "amount": 2790,
+    "date": "2013/01/21"
+}, {
+    "orderId": 8,
+    "region": "South America",
+    "country": "PRY",
+    "city": "Asuncion",
+    "amount": 3140,
+    "date": "2013/01/01"
+}, {
+    "orderId": 9,
+    "region": "Europe",
+    "country": "GBR",
+    "city": "London",
+    "amount": 6175,
+    "date": "2013/01/24"
+}, {
+    "orderId": 10,
+    "region": "Europe",
+    "country": "DEU",
+    "city": "Berlin",
+    "amount": 4575,
+    "date": "2013/01/11"
+}, {
+    "orderId": 11,
+    "region": "Europe",
+    "country": "ESP",
+    "city": "Madrid",
+    "amount": 3680,
+    "date": "2013/01/12"
+}];
+
+var layouts = [{
+    key: 0,
+    name: "Layout0"
+}, {
+    key: 1,
+    name: "Layout1"
+}, {
+    key: 2,
+    name: "Layout2"
+}];
